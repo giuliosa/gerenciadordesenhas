baf3f3cbb60a5b11476800989aa51fe1391cbc67
Giulio Sá <giuliocaetano@yahoo.com.br>
2016-02-29 01:36:00 -0300

    Design inicial da tela de login

---
+++ app/src/main/java/br/aeso/gerenciadordesenhas/LoginActivity.java
@@ -0,0 +1,17 @@
+package br.aeso.gerenciadordesenhas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by giulio on 10/12/15.
+ */
+public class LoginActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
